2dcf25e299e95944c5aaab08f7c3c39ae441e542
byplayer <byplayer100@gmail.com>
2011-03-01 18:22:43 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hinagiku::Application.config.secret_token = '8f12c157a13d96ba00bbe90bdb4a8dc5331981d40c13d279f0669a0bd0435367823cabe3bafb6a8bcc7835d297ad1775fa9c6633cf2c679a1e1e0edd67e204fe'
