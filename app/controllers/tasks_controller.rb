3c8426402a661ccf010e7ef6bcdd9abe47ad3616
byplayer <byplayer100@gmail.com>
2011-07-26 18:19:36 +0900

    update codes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,34 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.where(:done => false)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(params[:task])
+    redirect_to @task
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes params[:task]
+    redirect_to @task
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to :tasks
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,17 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+# -*- encoding: UTF-8 -*-
+names = [
+"切手を買う",
+"報告書を書く",
+"家賃を払う",
+"猫の餌を買う",
+"燃えないゴミを出す"
+]
+description = "これは説明です。" * 20
+5.times do |n|
+  Task.create(:name => names[n], :description => description,
+              :due_date => (n - 2).days.from_now, :done => n.zero?)
+end
