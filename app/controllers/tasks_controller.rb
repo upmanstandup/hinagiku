ea3962321f58a27080561536eb68a5a1aa3ef035
byplayer <byplayer100@gmail.com>
2011-08-09 18:23:25 +0900

    implements finish action
    I change redirect_to :back to redirect_to :tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -31,4 +31,10 @@ class TasksController < ApplicationController
     @task.destroy
     redirect_to :tasks
   end
+
+  def finish
+    @task = Task.find(params[:id])
+    @task.update_attribute(:done, true)
+    redirect_to :tasks
+  end
 end
